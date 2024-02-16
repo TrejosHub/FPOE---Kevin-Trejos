2af820000fe1432452747fa4c6a06f4f60605ec4
Kevin Trejos <trejos2005@outlook.com>
2024-02-15 20:45:48 -0500

    Mi Primer Commit

---
+++ strings.py
@@ -0,0 +1,12 @@
+#USO DE LAS MISMAS COMILLAS \ \
+cadena = "Esto es un \"ejemplo\" de cadena de texto"
+
+#TABULACIÓN \t
+cadena2= "Esto es un \t ejemplo de cadena de texto"
+
+#QUITAR ESPACIO \b
+cadena3= "Esto es un \bejemplo de cadena de texto"
+
+print(cadena)
+print(cadena2)
+print(cadena3)
